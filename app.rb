35b012d72ae47b06aee83be6892a7daae1cd3368
Amy Cen <amy.cen@blackrock.com>
2019-06-05 11:08:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+  
   get '/newteam' do
     erb :newteam
   end
